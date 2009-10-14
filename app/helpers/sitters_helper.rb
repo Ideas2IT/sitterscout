2e92938ca67bb54901f7183ff92a7357637de7b8
rajasekari2i <ranjeetrajasekar@gmail.com>
2009-10-14 16:26:05 +0530

    added method for url redirect

--- app/helpers/sitters_helper.rb
+++ app/helpers/sitters_helper.rb
@@ -5,5 +5,14 @@ module SittersHelper
     	  str.concat("<option value ='#{u.id}' #{selected.to_i==u.id ? "selected='selected'" : ""} email='#{u.email}' avatar='#{u.avatar_photo_url}'>#{u.profile.full_name}</option>")
     	end
     	return str
+ end
+ 
+ def referring_url
+    if request.env["HTTP_REFERER"]
+      return request.env["HTTP_REFERER"]
+    else
+      return "Empty"
     end
+  end
+ 
 end
