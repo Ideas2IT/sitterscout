09ee3767a49660394ac8a466d30a3b8c9d3642eb
rajasekari2i <ranjeetrajasekar@gmail.com>
2009-11-12 19:54:25 +0530

    duplicate index

--- db/migrate/046_add_index_id_removed_peoples.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexIdRemovedPeoples < ActiveRecord::Migration
-  def self.up
-    add_index :removed_peoples, :user_id
-  end
-
-  def self.down
-    remove_index :removed_peoples, :user_id
-  end
-end
