39fa6a9f827ab72756a1101de15065d79eb50f92
rajasekari2i <ranjeetrajasekar@gmail.com>
2009-11-21 12:36:41 +0530

    added migrate for active

---
+++ db/migrate/047_add_active_users.rb
@@ -0,0 +1,11 @@
+class AddActiveUsers < ActiveRecord::Migration
+  
+  def self.up
+    add_column :users, :active, :boolean, :default => false
+  end
+
+  def self.down
+    remove_column :users, :active 
+  end
+  
+end
