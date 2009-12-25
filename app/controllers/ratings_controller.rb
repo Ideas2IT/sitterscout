65a512ce9d5b5550527999f06d03eab7ab215163
rajasekari2i <ranjeetrajasekar@gmail.com>
2009-12-25 18:46:54 +0530

    changes in ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -14,13 +14,11 @@ class RatingsController < ApplicationController
                  
       sitter_no = SitterRating.find(:all,:conditions => ['sitter_id = ?',params[:sitter_id]])
       sum = @rating.my_request + @rating.arr_time + @rating.asked + @rating.condition + @rating.my_child
-      
       rating = User.find_by_id(params[:sitter_id])
 #      sitter_ratings.size > 0
       unless sitter_no.size.nil?
         if rating.rating_average <= 0.0 
             calc = (sum / 20)*5.to_f
-            calc = calc / sitter_no.size
         else
             calc = (sum / 20)*5.to_f
             calc = calc+rating.rating_average / sitter_no.size
