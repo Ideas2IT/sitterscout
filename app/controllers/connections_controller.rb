ff24974b7e91da781d4ab22d23c11cc1b040f5c6
rajasekari2i <ranjeetrajasekar@gmail.com>
2010-01-05 18:40:53 +0530

    changes for the view profile link in the parent login

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -61,6 +61,18 @@ class ConnectionsController < ApplicationController
     @profile = @user.profile
     render :layout => 'lightbox'
   end
+  
+  def view_friends_profile
+    @user = Parent.find_by_id(params[:id])
+    @profile = @user.profile
+    render :layout => 'lightbox'
+  end
+  
+  def view_sitter_profile
+    @user = Sitter.find_by_id(params[:id])
+    @profile = @user.profile
+    render :layout => 'lightbox'
+  end
 
   def show
     @user = User.find(params[:id])
